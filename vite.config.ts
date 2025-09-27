90da011af597776438cafd541f80e4924bc19c9f
mykola <mykola@spinplaygames.com>
2025-09-28 02:12:12 +0300

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/image-generator/",
   server: {
     allowedHosts: [
       '7a36a3d6c867.ngrok-free.app'
